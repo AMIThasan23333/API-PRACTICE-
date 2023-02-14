96c6e3af2f6ce5a7bd7a8cd44023757f0924faf9
My Name <cpgsom819@gmail.com>
2023-02-15 03:56:25 +0800

    loadDetails

--- Country.js
+++ Country.js
@@ -14,19 +14,19 @@ const country = countries => {
     // console.log(countries[0].name.common)
 
     const countryContainer  = document.getElementById('country');
-
       for (const country of countries){
 
         // console.log(country.name.common)
         const countryDiv  = document.createElement('div');
 
-        countryDiv.classList.add('country')
+        countryDiv.classList.add('countryi')
 
             countryDiv.innerHTML =   `
             
             <h3> Country Name : ${country.name.common} </h3>
 
             <h3> Country Capital : </h3>
+            <button  onclick = "loadDetails('${countries.cca2}')"> Country Detail</button>
 
             
             
@@ -39,6 +39,19 @@ const country = countries => {
 
 
     }
+
+// as it is string u have to quote  on both side of dynamic string
+
+//  <button  onclick = "loadDetails('${countries.cca2}')"> Country Detail</button>
+
+
+const loadDetails = (code) => {
+
+      const url = `https://restcountries.com/v3.1/alpha/${code}`
+
+      console.log(url)
+      
+}
 
 
 
